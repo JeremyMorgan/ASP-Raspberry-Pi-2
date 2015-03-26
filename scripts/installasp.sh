9d717ff4e232f4dc7764d5f477197fd6e9f4456a
Jeremy Morgan <jeremymorgan@gmail.com>
2015-03-25 22:21:25 -0700

    changing ASP install script

--- scripts/installasp.sh
+++ scripts/installasp.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+mkdir ~/sources
 mkdir ~/sources/aspnet5 
 cd ~/sources/aspnet5
 git clone git://github.com/aspnet/home.git
-sh ~/sources/aspnet5/kvminstall.sh
+sh ~/sources/aspnet5/home/kvminstall.sh
 source ~/.k/kvm/kvm.sh
 kvm upgrade
